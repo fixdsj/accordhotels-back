05a4443296a7de33db5797c1c4c670ffd2f06d65
narjisse.boulagsoa <boulagsoanarjiss@gmail.com>
2025-02-28 15:21:31 +0100

    add searchAll function and route to hotel controller and router

--- routes/hotelRouter.js
+++ routes/hotelRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {createHotel} from "../controllers/hotelController.js";
+import {createHotel,searchAll} from "../controllers/hotelController.js";
 import {authentication} from "../middlewares/authentication.js";
 
 const hotelRouter = express.Router();
 
 hotelRouter.post("/create", authentication, createHotel);
+hotelRouter.get("/search/:name:location:rating:price:description:picture:amenities:limit", searchAll);
 
 export default hotelRouter;
